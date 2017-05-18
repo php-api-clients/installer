0c4019f037d142110ab45c1f93913c7bd5a625bc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-05-18 22:43:07 +0200

    Added ls method to filesystem

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -26,4 +26,35 @@ class Filesystem
     {
         return (int)file_put_contents($filename, $contents);
     }
+
+    /**
+     * List all files in a directory and it's subdirectories.
+     *
+     * @param  string $path
+     * @return array
+     */
+    public function ls(string $path): array
+    {
+        $files = [];
+        $d = dir($path);
+        while (false !== ($entry = $d->read())) {
+            if (in_array($entry, ['.', '..'], true)) {
+                continue;
+            }
+            $entryPath = $path . $entry;
+            if (is_dir($entryPath)) {
+                foreach ($this->ls($entryPath . DIRECTORY_SEPARATOR) as $entryPath) {
+                    $files[] = $entryPath;
+                }
+                continue;
+            }
+            if (!is_file($entryPath)) {
+                continue;
+            }
+            $files[] = $entryPath;
+        }
+        $d->close();
+
+        return $files;
+    }
 }
--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -23,4 +23,30 @@ final class FilesystemTest extends TestCase
 
         self::assertSame(file_get_contents(__FILE__), file_get_contents($tmp));
     }
+
+    public function testLs()
+    {
+        $tmp = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('php-api-clients-installer-filesystem-ls-test-') . DIRECTORY_SEPARATOR;
+        mkdir($tmp);
+        mkdir($tmp . 'd' . DIRECTORY_SEPARATOR);
+
+        $fileOne = $tmp . 'one';
+        $fileTwo = $tmp . 'two';
+        $fileDOne = $tmp . 'd' . DIRECTORY_SEPARATOR . 'done';
+        touch($fileOne);
+        touch($fileTwo);
+        touch($fileDOne);
+
+        $filesystem = new Filesystem();
+        $files = $filesystem->ls($tmp);
+
+        self::assertSame(
+            [
+                $fileOne,
+                $fileTwo,
+                $fileDOne,
+            ],
+            $files
+        );
+    }
 }
